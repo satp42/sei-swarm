6cd60c981010455763e983b0a30f1aa9f9a8fb1a
Satwik Panigrahi <msg4satwik@gmail.com>
2025-08-17 16:00:23 -0700

    add base configs

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "@typescript-eslint/recommended",
+    "prettier",
+  ],
+  ignorePatterns: [
+    "dist",
+    ".eslintrc.cjs",
+    "node_modules",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+    "@typescript-eslint/explicit-function-return-type": "error",
+    "@typescript-eslint/no-explicit-any": "error",
+  },
+};
